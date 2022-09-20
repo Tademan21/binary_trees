aa4135f4e615987d4a7aabf06c1d53fbd5d40180
Tademan21 <101402625+Tademan21@users.noreply.github.com>
2022-09-20 10:33:45 +0300

    Update 123-avl_remove.c

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -113,4 +113,23 @@ bst_t *bst_remove(bst_t *root, int value)
 		if (type != 0)
 			bst_remove(root->right, type);
 	}
+	else
+		return (NULL);
+	return (root);
+}
 
+/**
+ * avl_remove - remove a node from a AVL tree
+ * @root: root of the tree
+ * @value: node with this value to remove
+ * Return: the tree changed
+ */
+avl_t *avl_remove(avl_t *root, int value)
+{
+	avl_t *root_a = (avl_t *) bst_remove((bst_t *) root, value);
+
+	if (root_a == NULL)
+		return (NULL);
+	bal(&root_a);
+	return (root_a);
+}
