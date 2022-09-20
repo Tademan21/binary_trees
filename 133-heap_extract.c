33adff944a6e3475e039826e632ed94653d12db3
Tademan21 <101402625+Tademan21@users.noreply.github.com>
2022-09-20 10:37:20 +0300

    Update 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -89,4 +89,108 @@ bst_t *swap(bst_t *a, bst_t *b)
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
+		return (0);
 
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * swap_head - helper func to swap head and node
+ * @head: pointer to head
+ * @node: pointer to node
+ * Return: pointer to severed head of tree
+ */
+heap_t *swap_head(heap_t *head, heap_t *node)
+{
+	if (node->parent->left == node)
+	{
+		node->parent->left = NULL;
+	} else
+		node->parent->right = NULL;
+	node->parent = NULL;
+	node->left = head->left;
+	node->right = head->right;
+	if (head->left)
+		head->left->parent = node;
+	if (head->right)
+		head->right->parent = node;
+	return (head);
+}
+
+/**
+ * perc_down - percolate head into correct position
+ * @node: pointer to head
+ * Return: pointer to head of tree
+ */
+heap_t *perc_down(heap_t *node)
+{
+	int max;
+	heap_t *next = node;
+
+	if (!node)
+		return (NULL);
+	max = node->n;
+	if (node->left)
+		max = MAX(node->left->n, max);
+	if (node->right)
+		max = MAX(node->right->n, max);
+	if (node->left && max == node->left->n)
+		next = node->left;
+	else if (node->right && max == node->right->n)
+		next = node->right;
+	if (next != node)
+	{
+		swap(node, next);
+		perc_down(node);
+	}
+	return (next);
+}
+
+/**
+ * heap_extract - extracts the root node of a Max Binary Heap
+ * @root: double pointer to root of tree
+ * Return: value stored in the root node
+ */
+int heap_extract(heap_t **root)
+{
+	size_t size, i;
+	char *binary, c, buffer[50];
+	int res;
+	heap_t *tmp, *head;
+
+	if (!root || !*root)
+		return (0);
+	SETUP_NODE_BLOC;
+	if (size == 1)
+	{
+		FREE_NODE_BLOC;
+		return (res);
+	}
+	do {
+		CONVERT_LOOP;
+	} while (size);
+
+	for (i = 1; i < strlen(binary); i++)
+	{
+		c = binary[i];
+		if (i == strlen(binary) - 1)
+		{
+			if (c == '1')
+			{
+				tmp = tmp->right;
+				break;
+			}
+			else if (c == '0')
+			{
+				tmp = tmp->left;
+				break;
+			}
+		}
+		if (c == '1')
+			tmp = tmp->right;
+		else if (c == '0')
+			tmp = tmp->left;
+	}
+	SWAP_HEAD_BLOC;
+	return (res);
+}
